ed1da5dd43b6064f8c367a0b51d1024d2b27b05a
Krista Bice <kbice@pro-kbice-m.corp.endurance.com>
2018-05-04 13:54:51 -0600

    "push up new vue files"

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
+import Router from 'vue'
 import App from './App.vue'
+import Carousel from './components/Carousel.vue'
+import Nav from './components/Nav.vue'
+import Home from './assets/pages/Home.vue'
+import About from './assets/pages/About.vue'
 
-Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/main.css'
 
+Vue.use(Router)
+
+Vue.component('my-nav', Nav);
+Vue.component('home-slider', Carousel);
+Vue.component('home', Home);
+Vue.component('about', About);
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    }
+  ]
+})
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
+
